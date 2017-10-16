2ce213f5fb37678ede871c1619bb67c166fe2502
Paulo Marques <pemsm@cin.ufpe.br>
2017-10-16 20:11:41 +0000

    ajuste de rotas entre home e informacoes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'information' => 'information#index'
-  get 'home/index'
+  get 'home' => 'home#index'
   
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
