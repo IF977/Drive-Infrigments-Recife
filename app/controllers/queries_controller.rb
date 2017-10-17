c552f8aa06b233b2f6389b44cd5f89c40d9ef254
Paulo Marques <pemsm@cin.ufpe.br>
2017-10-17 19:07:24 +0000

    paginas e rotas setadas #7 #8 #9 # 10

---
+++ app/controllers/queries_controller.rb
@@ -0,0 +1,4 @@
+class QueriesController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'contact' => 'contact#index'
+
+  get 'queries' => 'queries#index'
+
+  get 'team' => 'team#index'
+
   get 'information' => 'information#index'
+  
   get 'home' => 'home#index'
   
   root 'home#index'
